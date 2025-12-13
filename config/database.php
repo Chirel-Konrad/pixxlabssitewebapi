cdf9addaf14d8b457e1617f36f630264ab7de7e9
Chirel-Konrad <chirelkonrad@gmail.com>
2025-12-13 11:49:56 +0100

    Fix: Route 2FA matches documentation

--- config/database.php
+++ config/database.php
@@ -94,8 +94,11 @@ return [
     'prefix' => '',
     'prefix_indexes' => true,
     'search_path' => 'public',
-    'sslmode' => env('DB_SSLMODE', 'prefer'),
-],
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => [
+                PDO::ATTR_EMULATE_PREPARES => true,
+            ],
+        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
