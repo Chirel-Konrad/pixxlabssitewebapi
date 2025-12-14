9b2ab208885223bb07b5e4916c3b82ea7c84e65b
Chirel-Konrad <chirelkonrad@gmail.com>
2025-12-14 14:12:50 +0100

    Customize verification email in French

--- app/Notifications/QueuedVerifyEmail.php
+++ app/Notifications/QueuedVerifyEmail.php
@@ -5,10 +5,23 @@ namespace App\Notifications;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class QueuedVerifyEmail extends VerifyEmail implements ShouldQueue
 {
     use Queueable;
     
-    // We inherit toMail from parent VerifyEmail
+    /**
+     * Get the mail representation of the notification.
+     */
+    protected function buildMailMessage($url)
+    {
+        return (new MailMessage)
+            ->subject('Vérification de votre adresse email - Polariix')
+            ->greeting('Bonjour !')
+            ->line('Veuillez cliquer sur le bouton ci-dessous pour vérifier votre adresse email.')
+            ->action('Vérifier mon email', $url)
+            ->line('Si vous n\'avez pas créé de compte, aucune action n\'est requise.')
+            ->salutation('Cordialement, L\'équipe Polariix');
+    }
 }
